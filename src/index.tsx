b887d0f35255ab6745a0c737458d958b36884d13
yukimura-manase <masahiro19951212@gmail.com>
2022-10-23 18:39:12 +0900

    基本機能の実装完了

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // Redux-Provider
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
